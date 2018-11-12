21d463ce489be96b086f45c13b094291794d9c61
Atom Yuan (Wicresoft) <v-yuguip@microsoft.com>
2018-11-12 18:34:13 +0800

    navigator demo

---
+++ VueDemo/Day004/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <app-header></app-header>
+    </div>
+    <div class="container">
+      <router-view></router-view>
+    </div>
+   <div class="container">
+     <app-footer></app-footer>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Vue from 'vue'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  components:{
+    "app-header": Header,
+    "app-footer": Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
---
+++ VueDemo/Day004/src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+    <footer>
+        <p>{{copyRight}}</p>
+    </footer>
+</template>
+
+<script>
+    export default{
+        name:"",
+        data(){
+            return{
+                copyRight:"@CopyRight 2018 by Atom"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+footer{
+    padding: 10px 0;
+    background-color:#f8f9fa;
+}
+footer p{
+    color: #222;
+    margin: 0px;
+    padding: 0px;
+}
+</style>

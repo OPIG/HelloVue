9c830e009b9ba35644b8b565cd5dfd0aecc039fc
Atom Yuan (Wicresoft) <v-yuguip@microsoft.com>
2018-11-13 19:39:37 +0800

    vue-router

---
+++ VueDemo/Day004/src/components/about/contact/PhoneNumber.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="card-brand">
+       <h2>182384325423</h2>
+    </div>
+</template>
--- VueDemo/Day004/src/main.js
+++ VueDemo/Day004/src/main.js
@@ -8,6 +8,14 @@ import About from './components/about/About'
 import Login from './components/Login'
 import Register from './components/Register'
 
+//二级路由
+import Contact from './components/about/Contact'
+import Delivery from './components/about/Delivery'
+import History from './components/about/History'
+import OrderingGuide from './components/about/OrderingGuide'
+import PhoneNumber from  './components/about/contact/PhoneNumber'
+import PersonName from  './components/about/contact/PersonName'
+
 Vue.use(VueRouter);
 
 const router=new VueRouter({
@@ -15,13 +23,36 @@ const router=new VueRouter({
     {path:'/',name:"homeLink",component:Home},
     {path:'/menu',name:"menuLink",component:Menu},
     {path:'/admin',name:"adminLink",component:Admin},
-    {path:'/about',name:"aboutLink",component:About},
+    {path:'/about',name:"aboutLink",redirect:"/delivery",component:About,children:[
+      {path:'/history',name:"historyLink",component:History},
+      {path:'/about/contact',name:"contactLink",redirect:"/personname",component:Contact,children:[
+        {path:'/phonenumber',name:"phonNumberLink",component:PhoneNumber},
+        {path:'/personname',name:"personNameLink",component:PersonName}
+      ]},
+      {path:'/delivery',name:"deliveryLink",component:Delivery},
+      {path:'/orderingGuide',name:"orderingGuideLink",component:OrderingGuide},
+    ]},
     {path:'/login',name:"loginLink",component:Login},
     {path:'/register',name:"registerLink",component:Register},
     {path:'*',redirect:'/'}//default router
   ],
   mode:"history"
 });
+
+//全局守卫
+//判断store.gettes.isLogin === false
+router.beforeEach((to,from, next)=>{
+  console.log(to);
+  console.log(from);
+  console.log(next);
+
+  if(to.path == "/login" || to.path == "/register"){
+    next();
+  }else{
+    alert("还没有登陆, 请先登陆");
+    next("/login");
+  }
+})
 
 new Vue({
   el: '#app',
